225dd4e1480de7d81314218765690d12a649d4bd
Jose Lagos <mldesoj@hotmail.com>
2026-04-17 20:23:36 -0500

    version para vercel corregido

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // Separa node_modules en su propio chunk (vendor) automáticamente
-    splitVendorChunkPlugin(),
   ],
 
   build: {
